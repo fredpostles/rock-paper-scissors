9788fb5eeaf18fc5d73eb4958fb1de205acb10cd
Fred Postles <fredpostles@freds-mbp.home>
2022-08-30 12:37:42 +0100

    Updated JS file

---
+++ task.js
@@ -0,0 +1,32 @@
+function go(playerChoice) {
+  // define choices
+  rps = ["rock", "paper", "scissors"];
+  // generate a choice by the computer; a random selection
+  let computerChoice = rps[Math.floor(Math.random() * rps.length)];
+  console.log(`Computer choice: ${computerChoice}`);
+
+  // work out who won from either side, or a draw
+  if (playerChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (playerChoice === "rock") {
+    if (computerChoice === "paper") {
+      console.log("Computer wins :(");
+    } else {
+      console.log("You win!");
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "scissors") {
+      console.log("Computer wins :(");
+    } else {
+      console.log("You win!");
+    }
+  } else if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+      console.log("Computer wins :(");
+    } else {
+      console.log("You win!");
+    }
+  }
+}
+
+go("rock");
